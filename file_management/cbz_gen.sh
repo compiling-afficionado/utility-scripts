30d9489776e67e525ef13459681b11f9859ffcfd
compiling-afficionado <compiling-afficionado>
2025-04-12 22:55:58 +0100

    handling whitespace in paths

--- file_management/cbz_gen.sh
+++ file_management/cbz_gen.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+OIFS="$IFS"
+IFS=$'\n'
 for D in *; do
     if [ -d "${D}" ]; then
         echo "${D}"   # your processing here
-	zip ./${D}/${D}.cbz ./${D}/*.png ./${D}/*.jpg
+	zip ./${D}/${D}.cbz ./${D}/*.{png,jpg}
     fi
 done
+IFS="$OIFS"
