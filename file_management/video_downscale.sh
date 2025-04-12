27b99eaf5a505c57d4a645de36b3ef6c974d93c0
compiling-afficionado <compiling-afficionado>
2025-04-12 23:45:21 +0100

    video downscaling script

---
+++ file_management/video_downscale.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+OIFS="$IFS"
+IFS=$'\n'
+echo "Scanning directory..."
+for vid in *; do
+	if [[ "${vid}" == *.mov || "${vid}" == *.m4v || "${vid}" == *.mp4 ]]; then
+		size=$(ffprobe -v error -select_streams v:0 -show_entries stream=height,width -of csv=s=x:p=0 $vid)
+		echo "$vid - $size"
+		waste_left="${size%%x*}"
+		waste_right="${size#*x}"
+		if [ "$size" == '' ]; then
+			echo "Error getting size, skipping $vid"
+			continue
+		fi
+		if [ $waste_left -gt 1920 ] || [ $waste_right -gt 1920 ]; then
+			echo "$size is above threshold, checking for existing downscale..."
+			if [ ! -e ./downscale_"$vid" ]; then
+				if [ $waste_left -gt $waste_right ]; then
+					ffmpeg -i $vid -filter:v scale=-2:720 -c:a copy downscale_$vid
+				else
+					ffmpeg -i $vid -filter:v scale=720:-2 -c:a copy downscale_$vid
+				fi
+				echo "Downscaled file created: downscale_$vid"
+			else
+				echo "Downscale already exists: downscale_$vid"
+			fi
+		fi
+	fi
+done
+IFS="$OIFS"
