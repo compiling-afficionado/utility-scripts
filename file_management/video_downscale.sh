54980a9c176c6c0d31e531c7fcc82c1897cd829a
compiling-afficionado <compiling-afficionado>
2025-04-17 15:49:19 +0100

    less verbose ffmpeg output w/ time check progress using ffprobe

--- file_management/video_downscale.sh
+++ file_management/video_downscale.sh
@@ -16,9 +16,13 @@ for vid in *; do
 			echo "$size is above threshold, checking for existing downscale..."
 			if [ ! -e ./downscale_"$vid" ]; then
 				if [ $waste_left -gt $waste_right ]; then
-					ffmpeg -i $vid -filter:v scale=-2:720 -c:a copy downscale_$vid
+					length=$(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of csv=p=0 -sexagesimal $vid)
+					echo "TIME: $length"
+					ffmpeg -v quiet -stats -i $vid -filter:v scale=-2:720 -c:a copy downscale_$vid
 				else
-					ffmpeg -i $vid -filter:v scale=720:-2 -c:a copy downscale_$vid
+					length=$(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of csv=p=0 -sexagesimal $vid)
+					echo "TIME: $length"
+					ffmpeg -v quiet -stats -i $vid -filter:v scale=720:-2 -c:a copy downscale_$vid
 				fi
 				echo "Downscaled file created: downscale_$vid"
 			else
