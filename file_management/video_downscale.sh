df02a8417940059071a296ba4a7c813ced496ba3
compiling-afficionado <compiling-afficionado>
2025-04-26 08:24:58 +0100

    code refactor

--- file_management/video_downscale.sh
+++ file_management/video_downscale.sh
@@ -15,13 +15,11 @@ for vid in *; do
 		if [ $waste_left -gt 1920 ] || [ $waste_right -gt 1920 ]; then
 			echo "$size is above threshold, checking for existing downscale..."
 			if [ ! -e ./downscale_"$vid" ]; then
+				length=$(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of csv=p=0 -sexagesimal "$vid")
+				echo "TIME: $length"
 				if [ $waste_left -gt $waste_right ]; then
-					length=$(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of csv=p=0 -sexagesimal "$vid")
-					echo "TIME: $length"
 					ffmpeg -v quiet -stats -i "$vid" -filter:v scale=-2:720 -c:a copy "downscale_$vid"
 				else
-					length=$(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of csv=p=0 -sexagesimal "$vid")
-					echo "TIME: $length"
 					ffmpeg -v quiet -stats -i "$vid" -filter:v scale=720:-2 -c:a copy "downscale_$vid"
 				fi
 				echo "Downscaled file created: downscale_$vid"
